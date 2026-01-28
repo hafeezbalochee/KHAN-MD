665dccaec3979e8e9579baea12d137ae17f7b366
hafeezbalochee <hafeezbalochee@gmail.com>
2026-01-28 05:48:55 -0800

    Reset SESSION_ID in settings.js
    Updated SESSION_ID to an empty string for configuration.

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "https://mega.nz/file/nNQVFZgL#1Y0U2LTZ9Ql0vvuhNvfJ2aoV0lIwQiWqafCSNPmnyNI",  // add session Id 
+  SESSION_ID: "",  // add session Id 
   
   OWNER_NUMBER: "923003534702", // add owner number 
   
